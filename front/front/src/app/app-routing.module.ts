e7125eaa9d9f18cf753334464320d20ad67c79a0
fara0000 <fexri_imanzade@mail.ru>
2020-12-28 12:50:28 +0400

    main page with canvas and some layout and basic services done

--- front/front/src/app/app-routing.module.ts
+++ front/front/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginPageComponent } from "./pages/login-page/login-page.component";
-import { RegistrationPageComponent } from "./pages/registration-page/registration-page.component";
+import { RegistrationPageComponent } from './pages/registration-page/registration-page.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginPageComponent },
   { path: 'registration', component: RegistrationPageComponent },
+  { path: 'main', component: MainPageComponent},
   { path: '',   redirectTo: '/login', pathMatch: 'full' }
 ];
 
@@ -14,4 +16,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [ LoginPageComponent, RegistrationPageComponent ]
+export const routingComponents = [ LoginPageComponent, RegistrationPageComponent, MainPageComponent ];
--- front/front/src/app/app.module.ts
+++ front/front/src/app/app.module.ts
@@ -1,22 +1,33 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MainService } from './pages/main-page/main-page-service/main.service';
+import { LoginService } from './pages/login-page/login-page-service/login.service';
 import { AppComponent } from './app.component';
 import { ButtonModule } from 'primeng/button';
 import { BrowserModule } from '@angular/platform-browser';
 import { InputTextModule } from 'primeng/inputtext';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { RegistrationService } from './pages/registration-page/registration-page-service/registration.service';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    routingComponents
+    routingComponents,
   ],
   imports: [
     ButtonModule,
     BrowserModule,
     InputTextModule,
     AppRoutingModule,
+    InputNumberModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [
+    MainService,
+    LoginService,
+    RegistrationService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
