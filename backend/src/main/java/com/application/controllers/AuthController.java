946a0405d6cf13d1ce67035d21c7d48fd66bbe33
fakhriim <fakhriim@wizardsdev.com>
2021-01-08 18:35:12 +0400

    l

---
+++ backend/src/main/java/com/application/controllers/AuthController.java
@@ -0,0 +1,12 @@
+package com.application.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+public class AuthController {
+
+
+}
