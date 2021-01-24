f5b2291f08bd264edf5d4823b9d853f3fb4cfa86
Eragon35 <antipin35@yahoo.com>
2021-01-24 22:51:01 +0300

    Fix authentication build

--- backend/src/main/java/com/application/security/jwt/JwtUtils.java
+++ backend/src/main/java/com/application/security/jwt/JwtUtils.java
@@ -13,11 +13,9 @@ import java.util.Date;
 @Component
 public class JwtUtils {
 
-    @Value("${jwtSecret}")
-    private String jwtSecret;
 
-    @Value("${jwtExpirationMs}")
-    private int jwtExpirationMs;
+    private String jwtSecret = "VorovatPloho";
+    private int jwtExpirationMs = 100000000;
 
     public String generateJwtToken(Authentication authentication) {
 
@@ -37,15 +35,15 @@ public class JwtUtils {
             Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
             return true;
         } catch (SignatureException e) {
-//            log.error("Invalid JWT signature: {}", e.getMessage());
+            log.error("Invalid JWT signature: {}", e.getMessage());
         } catch (MalformedJwtException e) {
-//            log.error("Invalid JWT token: {}", e.getMessage());
+            log.error("Invalid JWT token: {}", e.getMessage());
         } catch (ExpiredJwtException e) {
-//            log.error("JWT token is expired: {}", e.getMessage());
+            log.error("JWT token is expired: {}", e.getMessage());
         } catch (UnsupportedJwtException e) {
-//            log.error("JWT token is unsupported: {}", e.getMessage());
+            log.error("JWT token is unsupported: {}", e.getMessage());
         } catch (IllegalArgumentException e) {
-//            log.error("JWT claims string is empty: {}", e.getMessage());
+            log.error("JWT claims string is empty: {}", e.getMessage());
         }
 
         return false;
--- backend/src/main/java/com/application/services/UserService.java
+++ backend/src/main/java/com/application/services/UserService.java
@@ -1,48 +1,33 @@
 package com.application.services;
 
-import lombok.extern.slf4j.Slf4j;
 import com.application.entities.User;
-import javax.transaction.Transactional;
-import com.application.security.jwt.JwtUtils;
 import com.application.repositories.UserRepository;
+import com.application.security.jwt.JwtUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Slf4j
 @Service
 public class UserService implements UserDetailsService {
-
-    private final JwtUtils jwtUtils;
+    final AuthenticationManager authenticationManager;
+    final JwtUtils jwtUtils;
     private final UserRepository userRepository;
-    private final AuthenticationManager authenticationManager;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder, AuthenticationManager authenticationManager, JwtUtils jwtUtils) {
-        this.jwtUtils = jwtUtils;
         this.userRepository = userRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
         this.authenticationManager = authenticationManager;
-    }
-
-
-
-    public boolean saveUser(User user) {
-        if(userRepository.findByUsername(user.getUsername()) != null){
-//            log.debug("User {} already exists in DB", user.getUsername());
-            return false;
-        } else {
-            user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
-            userRepository.save(user);
-//            log.debug("User {} saved in DB", user.getUsername());
-            return true;
-        }
+        this.jwtUtils = jwtUtils;
     }
 
     @Override
@@ -50,11 +35,22 @@ public class UserService implements UserDetailsService {
     public User loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username);
         if (user == null) {
-//            log.debug("User {} not found in DB", username);
+            log.debug("User {} not found in DB", username);
             throw new UsernameNotFoundException("User not found in DB");
         }
-//        log.debug("User {} found in DB", username);
+        log.debug("User {} found in DB", username);
         return user;
+    }
+
+    public boolean saveUser(User user) {
+        if (userRepository.findByUsername(user.getUsername()) != null) {
+            log.debug("User {} already exists in DB", user.getUsername());
+            return false;
+        }
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        log.debug("User {} saved in DB", user.getUsername());
+        return true;
     }
 
     public String getUserToken(User user) {
