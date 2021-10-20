cf787da6a14953d5fc1f235b9e6733faa3b0a0b7
Eragon35 <antipin35@yahoo.com>
2021-10-20 13:07:21 +0300

    Fix notifications

--- backend/src/main/java/com/application/mbeans/PointCounterMBean.java
+++ backend/src/main/java/com/application/mbeans/PointCounterMBean.java
@@ -3,26 +3,45 @@ package com.application.mbeans;
 import org.springframework.jmx.export.annotation.ManagedAttribute;
 import org.springframework.jmx.export.annotation.ManagedOperation;
 import org.springframework.jmx.export.annotation.ManagedResource;
+import org.springframework.jmx.export.notification.NotificationPublisher;
+import org.springframework.jmx.export.notification.NotificationPublisherAware;
 import org.springframework.stereotype.Component;
 
 import javax.management.Notification;
-import javax.management.NotificationBroadcasterSupport;
-
 import java.util.concurrent.atomic.AtomicLong;
 
 @ManagedResource
 @Component
-public class PointCounterMBean extends NotificationBroadcasterSupport {
+public class PointCounterMBean implements NotificationPublisherAware {
     private int pointCount = 0;
     private int hitCount = 0;
-
     private final AtomicLong notificationSequence = new AtomicLong();
+    private NotificationPublisher notificationPublisher;
+
 
     @ManagedOperation
     public void count(boolean income) {
         if (income) hitCount++;
-        else this.trigger();
+        else trigger();
         pointCount++;
+    }
+
+    @Override
+    public void setNotificationPublisher(NotificationPublisher notificationPublisher) {
+        this.notificationPublisher = notificationPublisher;
+    }
+
+    @ManagedOperation
+    public void trigger() {
+        if (notificationPublisher != null) {
+            final Notification notification = new Notification(
+                    "user.Missed",
+                    this,
+                    notificationSequence.getAndIncrement(),
+                    "Вы промазали!"
+            );
+            notificationPublisher.sendNotification(notification);
+        }
     }
 
     @ManagedAttribute
@@ -35,18 +54,9 @@ public class PointCounterMBean extends NotificationBroadcasterSupport {
         return pointCount;
     }
 
-    @ManagedOperation
-    public void trigger() {
-        System.out.println("Sending notification");
-        Notification notification = new Notification(
-                "UserMissed", this, notificationSequence.incrementAndGet(),
-                System.currentTimeMillis(), "You missed again"
-        );
-        this.sendNotification(notification);
-    }
-
     public void clear () {
         this.pointCount = 0;
         this.hitCount = 0;
+        this.notificationSequence.set(0);
     }
 }
